49b50bbd737c045364b5f91b7ddf7df548733fd3
King-blurr <ojoayobami65@gmail.com>
2023-02-18 03:58:11 -0800

    again

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
- * Return: Always return 0
- */
+ * Return: Always 0
+*/
 
 int main(void)
 {
-	int a, b, c, d, op1, op2;
+	int p, q;
 
-	a = b = c = d = 48;
-	while (d < 58)
+	for (p = 0; p <= 98; p++)
 	{
-		c = 48;
-		while (c < 58)
+		for (q = p + 1; q <= 99; q++)
 		{
-			b = 48;
-			while (b < 58)
-			{
-				a = 48;
-				while (a < 58)
-				{
-					op1 = (d * 10) + c;
-					op2 = (b * 10) + a;
-					if (op1 < op2)
-					{
-						putchar(d);
-						putchar(c);
-						putchar(' ');
-						putchar(b);
-						putchar(a);
-					if (d == 57 && c == 56 && b == 57 && a == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					a++;
-				}
-				b++;
-			}
-			c++;
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+
+			if (p == 98 && q == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		d++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
