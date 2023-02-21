4f33d06fd2e95b826559f563f78fb32bfcdb8a3a
King-blurr <ojoayobami65@gmail.com>
2023-02-21 14:55:26 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 97; alp <= 122; alp++)
+	{
+		_putchar(alp);
+
+	}
+	_putchar('\n');
+}
