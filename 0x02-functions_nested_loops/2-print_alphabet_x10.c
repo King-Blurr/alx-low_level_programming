ee03656218c55ed38101ba48262fbd5f11824f3b
King-blurr <ojoayobami65@gmail.com>
2023-02-21 16:26:14 -0800

    lasy digit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,12 @@ void print_alphabet_x10(void)
 {
 	int alp;
 	int br;
+
 	for (br = 0; br < 10; br++)
 	{
 		for (alp = 97; alp <= 122; alp++)
 		{
-			_putchar('alp');
+			_putchar(alp);
 		}
 		_putchar('\n');
 	}
