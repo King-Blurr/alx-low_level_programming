74a181f41832e706cfb4be36a48ff3b004a17b36
King-blurr <ojoayobami65@gmail.com>
2023-03-10 18:21:00 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("argc = %d\n", argc - 1);
+	return 0;
+}
