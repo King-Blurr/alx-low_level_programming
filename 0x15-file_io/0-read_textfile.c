aeaedccaa134d7f42a7d66d6be22e5396c9dcf0f
King-blurr <ojoayobami65@gmail.com>
2023-04-10 13:29:40 -0700

    Requiescat

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	close(file_descriptor);
+
+	free(buffer);
+
+	return (bytes_written);
+}
